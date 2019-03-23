6768b0ab89550a9f403e8eb74cd3b8dbbbd3415f
ravi8470 <rvkmr0851@gmail.com>
2019-03-24 01:04:32 +0530

    implemented the mail trigger to dummy email on newUserRegistered

---
+++ resources/views/emails/userRegistered.blade.php
@@ -0,0 +1,10 @@
+Hello!!
+
+<h3>Thanks for registering with us.</h3>
+<h3>Enjoy your stay.</h3>
+User Details:
+<h4>Name: {{$user->name}}</h4>
+<h4>Email: {{$user->email}}</h4>
+
+Warm regards,<br>
+abc
